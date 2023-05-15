613dcb2525a2188027e340cf19cb9e9f7a0fa35e
Oli Guei <olivrg@gmail.com>
2023-05-15 20:30:53 +0100

    Retrieve information

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask, request
+from langchain.chains.question_answering import load_qa_chain
 from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings import CohereEmbeddings
+from langchain.llms import OpenAI
 from langchain.vectorstores import Qdrant
 
 from flask_cors import CORS
+from qdrant_client import QdrantClient
 
 load_dotenv()
 openai_api_key = os.environ.get('openai_api_key')
@@ -35,3 +38,19 @@ def embed_pdf():
     qdrant = Qdrant.from_documents(docs, embeddings, url=qdrant_url, collection_name=collection_name, prefer_grpc=True, api_key=qdrant_api_key)
 
     return {"collection_name": qdrant.collection_name}
+
+
+@app.route('/retrieve', methods=['POST'])
+def retrieve_info():
+    collection_name = request.json.get("collection_name")
+    query = request.json.get("query")
+
+    client = QdrantClient(url=qdrant_url, prefer_grpc=True, api_key=qdrant_api_key)
+
+    embeddings = CohereEmbeddings(model="multilingual-22-12", cohere_api_key=cohere_api_key)
+    qdrant = Qdrant(client=client, collection_name=collection_name, embedding_function=embeddings.embed_query)
+    search_results = qdrant.similarity_search(query, k=2)
+    chain = load_qa_chain(OpenAI(openai_api_key=openai_api_key, temperature=0.2), chain_type="stuff")
+    results = chain({"input_documents": search_results, "question": query}, return_only_outputs=True)
+
+    return {"results": results["output_text"]}
